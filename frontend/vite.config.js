b089d5306e248e175f8384346e048220b9bedfce
abhinash129 <abhi.sah2016@gmail.com>
2025-08-21 16:23:10 +0530

    Initial commit of QuantumTrades project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,17 +8,36 @@
 
 
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import fs from "fs";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import fs from "fs";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     https: {
+//       key: fs.readFileSync("./localhost-key.pem"),
+//       cert: fs.readFileSync("./localhost.pem"),
+//     },
+//     port: 5173,
+//   },
+// });
+
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     https: {
-      key: fs.readFileSync("./localhost-key.pem"),
-      cert: fs.readFileSync("./localhost.pem"),
+      key: fs.readFileSync('./key.pem'),
+      cert: fs.readFileSync('./cert.pem'),
     },
     port: 5173,
-  },
-});
+    host: '0.0.0.0',
+  }
+})
+
